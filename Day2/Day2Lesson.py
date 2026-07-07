66b3b49028dd24c5f7082742801cbb12f716de32
AniketParmar <aniket336codes@gmail.com>
2026-07-07 10:30:56 +0530

    Update Day2Lesson.py

--- Day2/Day2Lesson.py
+++ Day2/Day2Lesson.py
@@ -1,1 +1,33 @@
+#Types of Comments
 
+#SingleLineComment
+
+"""
+This is
+a
+Multi
+Line
+Comment
+"""
+
+#If-Else
+
+a = 10
+
+if a == 10 :
+	print("Value of A is 10")
+
+elif a < 10 :
+	print("Value of A is less than 10")
+
+else :
+	print("Value of A is greater than 10")
+
+#For-Loop
+
+#printing tables
+
+for i in range(1,11,1):
+	print("10 X ",i,"",10 * i)
+
+#3rd value in bracket is for increament
